8ce62900ee3eb976b9d51ba6b02eb91d0e63edd1
Prateek Sreedhar Bharadwaj <prateek.sreedhar@gmail.com>
2017-11-27 07:55:02 -0500

    Update smunch.c

--- smunch.c
+++ smunch.c
@@ -10,28 +10,33 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
 unsigned long       flags;
 struct task_struct *process;
 
+// Get the process with PID = pid
 rcu_read_lock();
 process = pid_task(find_vpid(pid), PIDTYPE_PID);
 rcu_read_unlock();
 
+// Handle error case when the process is not found
 if( !process )
 	{
 	printk( KERN_ALERT "Process %d was not found\n", pid );
 	return -1;
 	}
 	
+// Handle error case when process is being traced
 if( unlikely(process->ptrace) )
 	{
 	printk( KERN_ALERT "Process %d is being traced\n", pid );
 	return -1;
 	}
 	
+// Handle case for multi-threaded processes 
 if( unlikely( !thread_group_empty( process ) ) )
 	{
 	printk( KERN_ALERT "Process %d is multithreaded\n", pid );
 	return -1;
 	}
 	
+// Handle case for Zombie process
 if( unlikely(process->exit_state == EXIT_ZOMBIE) )
 		{
 		printk(KERN_ALERT "Process %d is a zombie\n", pid);
@@ -50,6 +55,8 @@ if( !lock_task_sighand(process, &flags) )
 	printk( KERN_ALERT "Failed to grab the lock for process %d\n", pid );
 	return -1;
 	}
+
+// Send signal to the process
 process->signal->shared_pending.signal.sig[0] |= bit_pattern;	
 unlock_task_sighand(process, &flags);
 wake_up_process(process);
